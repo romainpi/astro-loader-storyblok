68345cd11e0976e8157cf119b7cbe5a5af6d3ab5
Romain Pironneau <929716+romainpi@users.noreply.github.com>
2025-09-20 20:35:12 +0200

    configure markdownlint
    according to https://github.com/github/markdownlint-github?tab=readme-ov-file#readme-ov-file

---
+++ .markdownlint-cli2.mjs
@@ -0,0 +1,12 @@
+import markdownIt from "markdown-it"
+import configOptions, {init} from "@github/markdownlint-github"
+const markdownItFactory = () => markdownIt({ html: true })
+const options = {
+    config: init(),
+    customRules: ["@github/markdownlint-github"],
+    markdownItFactory,
+    outputFormatters: [
+      [ "markdownlint-cli2-formatter-pretty", { "appendLink": true } ] // ensures the error message includes a link to the rule documentation
+    ]
+}
+export default options
