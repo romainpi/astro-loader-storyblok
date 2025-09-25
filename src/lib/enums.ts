9a2f6195506ee5ef52044dcfa8129ed569d7a371
Romain Pironneau <929716+romainpi@users.noreply.github.com>
2025-09-25 17:12:34 +0200

    Add sorting options for published dates to SortByEnum

--- src/lib/enums.ts
+++ src/lib/enums.ts
@@ -9,8 +9,12 @@
 export enum SortByEnum {
   CREATED_AT_ASC = "created_at:asc",
   CREATED_AT_DESC = "created_at:desc",
+  FIRST_PUBLISHED_AT_ASC = "first_published_at:asc",
+  FIRST_PUBLISHED_AT_DESC = "first_published_at:desc",
   NAME_ASC = "name:asc",
   NAME_DESC = "name:desc",
+  PUBLISHED_AT_ASC = "published_at:asc",
+  PUBLISHED_AT_DESC = "published_at:desc",
   SLUG_ASC = "slug:asc",
   SLUG_DESC = "slug:desc",
   UPDATED_AT_ASC = "updated_at:asc",
