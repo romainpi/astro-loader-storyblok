c084fcca32d78506ca08618aea3e2e1d61fda77e
Romain Pironneau <929716+romainpi@users.noreply.github.com>
2025-10-10 15:06:50 +0200

    Tweak playground

--- playground/minimal-astro-project/src/content.config.ts
+++ playground/minimal-astro-project/src/content.config.ts
@@ -1,5 +1,10 @@
 import { defineCollection } from "astro:content";
-import { SortByEnum, StoryblokLoader, type StoryblokLoaderCommonConfig } from "astro-loader-storyblok";
+import {
+  SortByEnum,
+  StoryblokLoader,
+  type StoryblokLoaderCommonConfig,
+  type StoryblokLoaderStoriesParameters,
+} from "astro-loader-storyblok";
 
 const accessToken = process.env.STORYBLOK_DELIVERY_PREVIEW_API_TOKEN;
 if (!accessToken) {
@@ -12,21 +17,33 @@ const sbContentLoaderConfig: StoryblokLoaderCommonConfig = {
 
 const storyblokLoader = new StoryblokLoader(sbContentLoaderConfig);
 
+const draftStoriesConfig: StoryblokLoaderStoriesParameters = {
+  storyblokParams: {
+    version: "draft",
+  },
+};
+
+// Draft collection: contains all your stories, including drafts
 const draftStories = defineCollection({
-  loader: storyblokLoader.getStoriesLoader({
-    storyblokParams: {
-      version: "draft",
-    },
-  }),
+  loader: storyblokLoader.getStoriesLoader(draftStoriesConfig),
 });
 
+const publishedStoriesConfig: StoryblokLoaderStoriesParameters = {
+  storyblokParams: {
+    version: "published",
+  },
+  sortBy: SortByEnum.FIRST_PUBLISHED_AT_DESC,
+};
+
+// Published collection: only published stories, sorted by first_published_at descending
 const publishedStories = defineCollection({
-  loader: storyblokLoader.getStoriesLoader({
-    storyblokParams: {
-      version: "published",
-      sort_by: SortByEnum.FIRST_PUBLISHED_AT_DESC,
-    },
-  }),
+  loader: storyblokLoader.getStoriesLoader(publishedStoriesConfig),
 });
 
 export const collections = { draftStories, publishedStories };
+
+// Exporting parameters for demonstration purposes (see: ./pages/index.astro)
+export const storyblokLoaderStoriesParameters = {
+  draftStoriesConfig,
+  publishedStoriesConfig,
+};
